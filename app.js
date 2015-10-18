42033cead6c6d5d6874c6909dbafb579b45b2ee7
jdrew5 <jdrew5@hotmail.com>
2015-10-18 10:55:53 -0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,79 @@
+// wait for the page to load
+$(document).ready(function() {
+
+	// declare variables to track salaries - total by month and and object for each
+	var totalMonthlySalary = 0;
+	var mySalaryObject = {
+		monthlySalary: 0
+	};
+
+	// listen for the submit button
+	$("#employeeinfo").submit(function(event) {
+		// prevent the submit from entering values into the URL
+		event.preventDefault();
+
+		// create object to hold name value pairs
+		var values={};
+
+		// load up the values object with names and entered values
+		$.each($("#employeeinfo").serializeArray(), function(i,field){
+			values[field.name]=field.value;
+		});
+
+		// clear out the text fields in the form
+		$("#employeeinfo").find("input[type=text]").val("");
+
+		// track the total monthly salary
+		totalMonthlySalary += Math.round(parseInt(values.salary)/12);
+		
+		// append entered values onto the DOM
+		appendDOM(values);
+
+		// display the total monthly salary in the form
+		$("#employeeinfo").find("#totalmonthlysalary").text("Total Monthly Salary: " + totalMonthlySalary);
+
+		// populate mySalaryObject with the monthly salary entered
+		mySalaryObject.monthlySalary = Math.round(parseInt(values.salary)/12);
+
+		// tie the salary object to the element
+		$("#employeecontainer").children().last().data(mySalaryObject);
+
+	});
+
+	// listen for the employee remove button to fire
+	$("#employeecontainer").on('click', '.removeButton', function() {
+
+		// pull the salary of the employee from the object that was attached to the element
+		var minusSalary = $(this).parent().data("monthlySalary");
+
+		// decrease the total salary by the employee's salary
+		totalMonthlySalary = totalMonthlySalary - minusSalary;
+		
+		// write out the updated total monthly salary to the form
+		$("#employeeinfo").find("#totalmonthlysalary").text("Total Monthly Salary: " + totalMonthlySalary);
+
+    	// remove the employee from the display
+    	$(this).parent().remove();
+  });
+
+});
+
+function appendDOM(employee){
+	// add the employee div
+	$("#employeecontainer").append("<div class='employee'></div>");
+
+	// get the element of the last div
+	var $el = $("#employeecontainer").children().last();
+
+	// append object values
+	$el.append("<p>First Name: "+employee.firstname+"</p>");
+	$el.append("<p>Last Name:"+employee.lastname+"</p>");
+	$el.append("<p>ID Number:"+employee.idnumber+"</p>");
+	$el.append("<p>Job Title:"+employee.jobtitle+"</p>");
+	$el.append("<p>Salary: "+employee.salary+"</p>");
+
+	// add the remove button	
+	$el.append("<button class='removeButton'>Remove</button>");
+
+};
+
